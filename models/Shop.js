e9239933b49c53a557e881ed5bbf4faa79521f9c
Matheus De O Zacanini <zacanini2@gmail.com>
2025-01-16 14:25:14 -0300

    DB: adicionando a coluna OauthId na tabela shops

--- models/Shop.js
+++ models/Shop.js
@@ -1,31 +1,35 @@
 module.exports = (sequelize, DataTypes) => {
-    const Shop = sequelize.define('Shop', {
-      id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
-      },
-      nome: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      numeroDeFuncionarios: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-      },
-      horaAbertura: {
-        type: DataTypes.TIME,
-        allowNull: false
-      },
-      horaDeFechamento: {
-        type: DataTypes.TIME,
-        allowNull: false
-      }
-    }, {});
-    Shop.associate = function(models) {
-      Shop.hasMany(models.Barber, { foreignKey: 'idShop' });
-      Shop.hasMany(models.Servico, { foreignKey: 'idShop' });
-      Shop.hasMany(models.Agenda, { foreignKey: 'idShop' });
-    };
-    return Shop;
+  const Shop = sequelize.define('Shop', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    oauthId: {
+      type: DataTypes.STRING,
+      unique: true
+    },
+    nome: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    numeroDeFuncionarios: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    horaAbertura: {
+      type: DataTypes.TIME,
+      allowNull: false
+    },
+    horaDeFechamento: {
+      type: DataTypes.TIME,
+      allowNull: false
+    }
+  }, {});
+  Shop.associate = function(models) {
+    Shop.hasMany(models.Barber, { foreignKey: 'idShop' });
+    Shop.hasMany(models.Servico, { foreignKey: 'idShop' });
+    Shop.hasMany(models.Agenda, { foreignKey: 'idShop' });
   };
+  return Shop;
+};
